955666797d62e580621286cec210cbd5b6fb99aa
Gerardo Flores-Iga <139929491+gerapskx@users.noreply.github.com>
2024-08-27 14:33:10 -0400

    Add files via upload

---
+++ TRP-V_Phylogeny_script.R
@@ -0,0 +1,22 @@
+###TRP-V phylogeny###
+
+###Libraries to use####
+
+library(ape)
+
+TRP_V_tree <- ape::read.tree("TRP_V_Phylogeny_Newick")
+
+dendogramTRPV <- chronos(TRPVtree)
+
+is.ultrametric(dendogramTRPV)
+
+plot(dendogramTRPV,
+     edge.color = "gray",               
+     tip.color = "black",                
+     cex = 1,                           
+     lwd = 4,                             
+     show.tip.label = TRUE,               
+     font = 1,                            
+     edge.width = 2,                      
+     align.tip.label = TRUE,             
+     type = "radial", no.margin = TRUE)
